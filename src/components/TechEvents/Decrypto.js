c0238a1cd23b7d1c97ffa8055dd4aa4681dd1018
Varun Kumar <varunkumarsunkaraboina@gmail.com>
2025-02-10 16:18:36 +0530

    first commit

--- src/components/TechEvents/Decrypto.js
+++ src/components/TechEvents/Decrypto.js
@@ -37,7 +37,7 @@ const Decrypto=()=> {
           <h2 className="text-[20px] sm:text-[24px] font-semibold mb-4 bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 bg-clip-text text-transparent">Event Details</h2>
           <div className="text-sm space-y-2">
             <p className="flex items-center gap-1 font-kittu"><Calendar size={24} className='text-blue-500'/><strong className='font-kittu'>Date :</strong> Feb 24</p>
-            <p className="flex items-center gap-1 font-kittu"><Timer size={24} className='text-blue-500'/><strong className='font-kittu'>Duration :</strong>30 minutes</p>
+            <p className="flex items-center gap-1 font-kittu"><Timer size={24} className='text-blue-500'/><strong className='font-kittu'>Duration :</strong>1 hour</p>
             <p className="flex items-center gap-1 font-kittu"><Clock size={24} className='text-blue-500'/><strong className='font-kittu'>Time :</strong>2pm</p>
             <p className="flex items-center gap-1 font-kittu">
               <NotebookPen  size={24} className='text-blue-500'/>
